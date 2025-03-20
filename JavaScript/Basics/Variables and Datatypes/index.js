3fafe362b5f0b6212d74faa8aa231990773b0e11
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-20 22:36:26 +0530

    JavaScript varibles and datatypes

---
+++ JavaScript/Basics/Variables and Datatypes/index.js
@@ -0,0 +1,33 @@
+console.log("Aviral Varshney"); //Go live krk inspect pe console pe jaakr milega ye likha hua
+myname= "Aviral Varshney";
+console.log(myname);
+age = 19;
+cgpa = 8.9;
+//let is block scoped, cannot be redeclared but can be updated
+//const is block scoped, cannot be redeclared or updated
+//var is globally scoped, can be redeclared and updated
+let x=null;
+let y=undefined;
+value = true;
+let fullName = "Aviral Varshney"; //let cannot be redclared but can be updated. (Global Scoped)
+let a = 10;
+console.log(typeof a); //number
+console.log(typeof x);   //object
+console.log(typeof y);   //undefined
+console.log(typeof value);   //boolean
+console.log(typeof fullName);   //string
+console.log(typeof cgpa);   //number
+let big  = BigInt(1234567890123456789012345678901234567890);
+console.log(big);
+console.log(typeof big);   //bigint
+const student = {
+    Name : 'Aviral Varshney',
+    Age : 19,
+    CGPA : 8.9,
+    isMale : true,
+};
+console.log(student);
+console.log(typeof student);   //object
+console.log(student.Name);   //Aviral Varshney
+student.Age+=1;
+console.log(student["Age"]);   //20
