9d6b8861e9838cd10da2645b8bcc0d2629985e18
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-20 22:57:42 +0530

    Alert and Prompt

---
+++ JavaScript/Basics/Take Prompt/index.js
@@ -0,0 +1,8 @@
+/* Prompt and alert:
+
+Alert is a message, displayed as a popup and is one-time
+Prompt is also a message but can also have some message and be saved to a variable
+*/
+alert("You will be asked your name!!!")
+let fullName = prompt("Enter your name: ");
+console.log(fullName);
