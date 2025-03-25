78391ed6b72bd60da313e4ca08e332bf95681cef
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-25 22:52:35 +0530

    Learned about JSON and API

---
+++ JavaScript/Basics/Objects/Object De structure and JSON/index.js
@@ -0,0 +1,20 @@
+const course = {
+    coursename: 'javascript',
+    price: 1000,
+    duration: '3 months',
+};
+const{coursename:name}=course;
+console.log(name); //javascript
+
+
+//API : Application Programming Interface, apna kaam kisi aur k sir pr daalna
+//JSON : Javascript Object Notation
+//pehle backend se jo data aata tha wo xml mei hota tha ab JSON mei aata hai
+//XML : Extensible Markup Language
+// {
+//     "name":"Aviral",
+//     "age":19,
+//     "email":"aviral@varshney.com"
+// } isko object k form mei convert krk kaam krte hai
+//kabhi kabhi arrays k form mei bhi milta hai
+
